3a4f2ee940082aaa0b551d9a067fcd7b1f5f2b9c
naruepol <naruapon@gmail.com>
2017-08-22 15:32:14 +0700

    Initial commit

---
+++ src/ListTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list1 = new ArrayList<>();
+		list1.add("Kullanard");
+		list1.add("Sittichok");
+		list1.add("Pattamaporn");
+		
+		for(String list:list1){
+			System.out.println(list);
+		}
+		
+		Collections.sort(list1);
+		
+		for(String list:list1){
+			System.out.println(list);
+		}		
+	}
+}
